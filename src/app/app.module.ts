405b2351e8c77d0fdee3630365e185f13b6f2e05
Aleksei Rogozin <alrogozin@gmail.com>
2018-04-03 15:53:36 +0300

    030418

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppBoostrapModule } from './app-boostrap/app-boostrap.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent
   ],
   imports: [
-    BrowserModule, AppBoostrapModule
+    BrowserModule,
+    AppBoostrapModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'main',
+        component: MainpageComponent
+      }
+      /*,
+      {
+        path: 'editnewrecipe',
+        component: EditNewRecipeComponent
+      },
+      {
+        path: '',
+        redirectTo: '/recipes',
+        pathMatch: 'full'
+      },
+      {
+        path: 'recipes/:recipe_id',
+        component: RecipeDetailsComponent
+      },
+      */
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
